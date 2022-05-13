f1476bee2fd2192f263545e37ebe487fd38135f3
andrii-zhuk <2ndriy@gmail.com>
2022-05-13 11:58:33 +0300

    change node canvas

--- webApp/src/components/CanvasObjects/nodeCanvas.tsx
+++ webApp/src/components/CanvasObjects/nodeCanvas.tsx
@@ -1,30 +1,118 @@
 import { NodeObject } from "force-graph";
 
-export function getNodeCanvas(
+function basicCanvas(
+  node: NodeObject,
+  ctx: CanvasRenderingContext2D,
+  globalScale: number,
+  label: string,
+  bkgColor: string,
+  textColor: string
+) {
+  let fontSize = 18 / globalScale;
+  ctx.font = `${fontSize}px Sans-Serif`;
+  let textWidth = ctx.measureText(label).width;
+  const bckgDimensions = [textWidth, fontSize].map((n) => n + fontSize * 0.3); // some padding
+
+  if (bkgColor != null) {
+    ctx.fillStyle = bkgColor;
+    ctx.fillRect(
+      node.x - bckgDimensions[0] / 2,
+      node.y - bckgDimensions[1] / 2,
+      bckgDimensions[0],
+      bckgDimensions[1]
+    );
+  }
+
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillStyle = textColor;
+  ctx.fillText(label, node.x, node.y);
+}
+
+export function defaultNodeCanvas(
   node: NodeObject,
   ctx: CanvasRenderingContext2D,
   globalScale: number
 ) {
-  const label = `${node.id}`;
-  let fontSize = 15 / globalScale;
+  basicCanvas(
+    node,
+    ctx,
+    globalScale,
+    node.id.toString(),
+    "rgba(255, 255, 255, 0.8)",
+    "black"
+  );
+}
+
+export function sourceNodeCanvas(
+  node: NodeObject,
+  ctx: CanvasRenderingContext2D,
+  globalScale: number
+) {
+  basicCanvas(node, ctx, globalScale, "Source", "lightblue", "white");
+}
+
+export function sinkNodeCanvas(
+  node: NodeObject,
+  ctx: CanvasRenderingContext2D,
+  globalScale: number
+) {
+  basicCanvas(node, ctx, globalScale, "Sink", "purple", "white");
+}
+
+export function inactiveNodeCanvas(
+  node: NodeObject,
+  ctx: CanvasRenderingContext2D,
+  globalScale: number
+) {
+  basicCanvas(node, ctx, globalScale, node.id.toString(), "lightgray", "white");
+}
+
+export function excessChangedNodeCanvas(
+  node: NodeObject,
+  ctx: CanvasRenderingContext2D,
+  globalScale: number,
+  excess: number
+) {
+  if (excess < 0) {
+    basicCanvas(
+      node,
+      ctx,
+      globalScale,
+      node.id.toString(),
+      "lightgreen",
+      "black"
+    );
+  } else {
+    basicCanvas(
+      node,
+      ctx,
+      globalScale,
+      node.id.toString(),
+      "rgba(255, 255, 255, 0.8)",
+      "black"
+    );
+  }
+  let label = `+ ${excess}`;
+  let textColor = "green";
+  if (excess < 0) {
+    label = `- ${-excess}`;
+    textColor = "red";
+  }
+  let fontSize = 18 / globalScale;
   ctx.font = `${fontSize}px Sans-Serif`;
   let textWidth = ctx.measureText(label).width;
-  const bckgDimensions = [textWidth, fontSize].map((n) => n + fontSize * 0.2); // some padding
-  // textWidth += fontSize * 0.2;
-  // fontSize += fontSize * 0.2;
+  const bckgDimensions = [textWidth, fontSize].map((n) => n + fontSize * 0.3); // some padding
 
-  ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+  ctx.fillStyle = "rgba(255, 255, 255, 0.7)";
   ctx.fillRect(
     node.x - bckgDimensions[0] / 2,
-    node.y - bckgDimensions[1] / 2,
-    textWidth,
-    fontSize
+    node.y - (3 * bckgDimensions[1]) / 2,
+    bckgDimensions[0],
+    bckgDimensions[1]
   );
-
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
-  ctx.fillStyle = "black";
-  ctx.fillText(label, node.x, node.y);
-
-  // node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+  ctx.fillStyle = textColor;
+  ctx.fillText(label, node.x, node.y - bckgDimensions[1]);
 }
--- webApp/src/components/DisplayGraph/DisplayGraph.tsx
+++ webApp/src/components/DisplayGraph/DisplayGraph.tsx
@@ -3,7 +3,13 @@ import { ForceGraph2D } from "react-force-graph";
 import { genRandomTree } from "../../datasets/random-data";
 import { DirectedGraph } from "../../types/DirectedGraph";
 import { getGraph } from "../../types/HighlightedGraph";
-import { getNodeCanvas } from "../CanvasObjects/nodeCanvas";
+import {
+  excessChangedNodeCanvas,
+  defaultNodeCanvas,
+  inactiveNodeCanvas,
+  sinkNodeCanvas,
+  sourceNodeCanvas,
+} from "../CanvasObjects/nodeCanvas";
 import { Algorithm } from "../../types/Algorithm";
 import { GraphData, LinkObject, NodeObject } from "force-graph";
 export default function useDisplayGraph(props: {
@@ -14,7 +20,11 @@ export default function useDisplayGraph(props: {
   step: number;
 }) {
   const [directedGraph, setDirectedGraph] = React.useState<DirectedGraph>(null);
-  const [graphData, setGraphData] = React.useState<GraphData>(getGraph(null));
+  const [graphData, setGraphData] = React.useState<{
+    data: GraphData;
+    nodeTranslator: Map<string, number>;
+    linkTranslator: Map<string, number>;
+  }>(getGraph(null));
   const [algorithm, setAlgorithm] = React.useState<Algorithm>({ steps: [] });
   const [prevStep, setPrevStep] = React.useState<number>(null);
   const forceGraphRef = React.useRef();
@@ -27,23 +37,55 @@ export default function useDisplayGraph(props: {
   }, [props.algorithm]);
 
   const [highlightNodes, setHighlightNodes] = React.useState(new Set());
+  const [excessGain, setExcessGain] = React.useState(new Map());
+
   const [highlightLinks, setHighlightLinks] = React.useState(new Set());
+
+  function stringify(link: LinkObject): string {
+    const src = link.source;
+    const trg = link.target;
+    const source_id =
+      typeof src === "number" || typeof src === "string"
+        ? src.toString()
+        : src.id.toString();
+    const target_id =
+      typeof trg === "number" || typeof trg === "string"
+        ? trg.toString()
+        : trg.id.toString();
+    return JSON.stringify({ source: source_id, target: target_id });
+  }
+
+  function isHighlightedLink(link: LinkObject): boolean {
+    return highlightLinks.has(stringify(link));
+  }
+
+  function isHighlightedNode(node: NodeObject): boolean {
+    return highlightNodes.has(node.id.toString());
+  }
 
   const updateHighlight = () => {
     setHighlightNodes(highlightNodes);
     setHighlightLinks(highlightLinks);
+    setExcessGain(excessGain);
   };
 
   if (props.step !== prevStep && props.step !== null) {
     if (directedGraph !== null && algorithm !== null) {
       highlightNodes.clear();
       highlightLinks.clear();
+      excessGain.clear();
 
-      algorithm.steps[props.step].nodes_affected.forEach((node) =>
-        highlightNodes.add(directedGraph.nodes[node.node_id].label)
-      );
+      algorithm.steps[props.step].nodes_affected.forEach((node) => {
+        directedGraph.nodes[node.node_id].excess += node.excess_amount;
+        highlightNodes.add(directedGraph.nodes[node.node_id].label);
+        excessGain.set(
+          directedGraph.nodes[node.node_id].label,
+          node.excess_amount
+        );
+      });
       algorithm.steps[props.step].edges_affected.forEach((e) => {
         const edge = directedGraph.edges_list[e.edge_id];
+        edge.flow += e.flow_amount;
         highlightLinks.add(
           JSON.stringify({
             source: directedGraph.nodes[edge.from_id].label,
@@ -59,32 +101,71 @@ export default function useDisplayGraph(props: {
   return (
     <ForceGraph2D
       ref={forceGraphRef}
-      graphData={graphData}
+      graphData={graphData.data}
       // height={props.height}
       // width={props.width}
-      nodeLabel={(node: any) => node.id}
-      nodeCanvasObject={getNodeCanvas}
-      linkWidth={(link) =>
-        highlightLinks.has(
-          JSON.stringify({ source: link.source.id, target: link.target.id })
-        )
-          ? 5
-          : 1
-      }
+      linkWidth={(link) => (isHighlightedLink(link) ? 4 : 2)}
       linkDirectionalParticles={4}
-      linkDirectionalParticleWidth={(link) =>
-        highlightLinks.has(
-          JSON.stringify({ source: link.source.id, target: link.target.id })
-        )
-          ? 4
-          : 0
-      }
-      nodeCanvasObjectMode={(node) =>
-        highlightNodes.has(`${node.id}`) ? "replace" : "after"
-      }
-      linkCurvature={0.05}
+      linkDirectionalParticleWidth={(link) => (isHighlightedLink(link) ? 4 : 0)}
+      linkCurvature={0.08}
       linkDirectionalArrowLength={3}
-      warmupTicks={10000}
+      linkLabel={(link: LinkObject) => {
+        const id = graphData.linkTranslator.get(stringify(link));
+        if (id !== undefined) {
+          const { flow, capacity, amplification } =
+            directedGraph.edges_list[id];
+          if (capacity !== 0) return `${flow}/${capacity} x ${amplification}`;
+        }
+        return "";
+      }}
+      linkCanvasObject={(
+        link: LinkObject,
+        ctx: CanvasRenderingContext2D,
+        globalScale: number
+      ) => {
+        // todo()
+      }}
+      linkCanvasObjectMode="after"
+      // warmupTicks={5000}
+      nodeCanvasObject={(
+        node: NodeObject,
+        ctx: CanvasRenderingContext2D,
+        globalScale: number
+      ) => {
+        const id = graphData.nodeTranslator.get(node.id.toString());
+        if (id === directedGraph.source) {
+          sourceNodeCanvas(node, ctx, globalScale);
+          return;
+        }
+        if (id === directedGraph.sink) {
+          sinkNodeCanvas(node, ctx, globalScale);
+          return;
+        }
+        if (!directedGraph.nodes[id].reachable_from_source) {
+          inactiveNodeCanvas(node, ctx, globalScale);
+          return;
+        }
+        if (isHighlightedNode(node)) {
+          const excess = excessGain.get(node.id);
+          // otherwise we will use default canvas
+          if (excess !== undefined) {
+            excessChangedNodeCanvas(node, ctx, globalScale, excess);
+            return;
+          }
+        }
+        defaultNodeCanvas(node, ctx, globalScale);
+      }}
+      nodeLabel={(node: NodeObject) => {
+        const id = graphData.nodeTranslator.get(node.id.toString());
+        if (id === directedGraph.source) {
+          return "Source";
+        }
+        if (id === directedGraph.sink) {
+          return "Sink";
+        }
+        if (!directedGraph.nodes[id].reachable_from_source) return "";
+        return `Excess: ${directedGraph.nodes[id].excess}`;
+      }}
     />
   );
 }
